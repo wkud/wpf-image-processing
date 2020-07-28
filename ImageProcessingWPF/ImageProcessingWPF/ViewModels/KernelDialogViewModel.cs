e433aa399e778c26a84ed4b24e63b220cdac57e2
Wojtek <vopox07@gmail.com>
2020-07-28 22:33:46 +0200

    Clean up layout. Divide large views/viewModels into smaller

--- ImageProcessingWPF/ImageProcessingWPF/ViewModels/KernelDialogViewModel.cs
+++ ImageProcessingWPF/ImageProcessingWPF/ViewModels/KernelDialogViewModel.cs
@@ -1,85 +1,22 @@
-﻿using ImageProcessingWPF.Utility;
+﻿using ImageProcessingWPF.Models;
 using ImageProcessingWPF.Views;
-using System.Collections.Generic;
-using System.ComponentModel;
 
 namespace ImageProcessingWPF.ViewModels
 {
-    class KernelDialogViewModel : INotifyPropertyChanged
+    class KernelDialogViewModel
     {
-        public class Cell 
-        {
-            private int _value;
-            public string Value
-            {
-                get { return _value.ToString(); }
-                set { int.TryParse(value, out _value); }
-            }
-        }
-        public class Row
-        {
-            public List<Cell> Cells { get; set; } = new List<Cell>();
-        }
-
-        private List<Row> _matrix;
-        public List<Row> Matrix //TODO explain why this is needed instead of List<List<string>>
-        {
-            get { return _matrix; }
-            set
-            {
-                _matrix = value;
-                PropertyChanged.Notify(this, "Matrix");
-            }
-        }
-
-
-        private int _width = 3;
-        public string Width
-        {
-            get { return _width.ToString(); }
-            set
-            {
-                int.TryParse(value, out _width);
-                InitializeGrid(); //TODO optimise
-            }
-        }
-
-        private int _height = 3;
-        public string Height
-        {
-            get { return _height.ToString(); }
-            set
-            {
-                int.TryParse(value, out _height);
-                InitializeGrid(); //TODO optimise
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
+        private DynamicMatrixHandler _matrixHandler;
+        public DynamicMatrixHandler MatrixHandler => _matrixHandler;
 
         public KernelDialogViewModel(MainWindow parentView)
         {
             KernelDialogWindow dialogWindow = new KernelDialogWindow();
             dialogWindow.DataContext = this;
             dialogWindow.Owner = parentView;
-            InitializeGrid();
+
+            _matrixHandler = new DynamicMatrixHandler();
             dialogWindow.ShowDialog();
         }
-        private void InitializeGrid()
-        {
-            var grid = new List<Row>();
-            for (int i = 0; i < _width; i++)
-            {
-                grid.Add(new Row());
 
-                for (int j = 0; j < _height; j++)
-                {
-                    var value = new Cell();
-                    value.Value = (i * 10 + j).ToString();
-                    grid[i].Cells.Add(value);
-                }
-            }
-            Matrix = grid;
-        }
     }
 }
